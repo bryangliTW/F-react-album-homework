6912c7e3c6b3dc7b7eb0b5d2e03c3919f1678803
Bryan Li <yang.li@thoughtworks.com>
2020-11-02 16:24:47 +0800

    added a test for the welcome message

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -4,9 +4,16 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-test('should render a title in ht header section', () => {
+test('should render a title in the header section', () => {
   const mockUser = { name: 'Bryan', userName: 'bryannnnn' };
   render(<Header user={mockUser} />);
 
   expect(screen.getByText("Bryan's Album")).toBeTruthy();
+});
+
+test('should render a welcome message in the header section', () => {
+  const mockUser = { name: 'Bryan', username: 'bryannnnn' };
+  render(<Header user={mockUser} />);
+
+  expect(screen.getByText('Hi, bryannnnn')).toBeTruthy();
 });
